caff847c539d7af208250b491610809a599148fd
nurqalik <qaliksulistyo@gmail.com>
2024-04-19 02:31:15 +0700

    Update

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -5,12 +5,19 @@ export default async function FullImages(props: { id: number }) {
   const image = await getImage(props.id);
 
   return (
-    <Image
-      src={image.url}
-      alt={image.name}
-      width={480}
-      height={480}
-      className="w-96 object-cover"
-    />
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex-shrink flex items-center justify-center">
+      <Image
+        src={image.url}
+        alt={image.name}
+        width={1000}
+        height={1000}
+        className="flex-shrink object-contain"
+      />
+      </div>
+      <div className="flex w-48 flex-col flex-shrink-0 border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
   );
 }
